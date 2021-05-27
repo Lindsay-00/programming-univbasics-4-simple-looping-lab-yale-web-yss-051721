a3114c051e08618e9543d2f459e73bee74c9fa99
Lindsay Chen <lindsay.chen@yale.edu>
2021-05-27 18:24:26 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -21,7 +21,7 @@ output_array (y)
 def return_string_array (array)
   counter = 0 
   while counter < array.length do
-    array [counter].to_s
+    array [counter] = array [counter].to_s
     counter += 1 
   end
   array
